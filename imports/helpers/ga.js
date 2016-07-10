b97237e50ea0a6bc2173458054e73ee22128517b
Matt Durr <matt@onedurr.com>
2016-07-10 14:09:45 -0400

    remove default ga and refine message if custom one not added; also, only inject if added

--- imports/helpers/ga.js
+++ imports/helpers/ga.js
@@ -1,17 +1,20 @@
 
-
 export let TrackingCode = () => {
-  if (console) console.log("ADD YOUR GOOGLE ANALYTICS TRACKING CODE HERE (/imports/helpers/ga.js)")
-  if (typeof document !== 'undefined') {
+  var ga_id = 'UA-xxx-yy'; // ADD YOUR GOOGLE ANALYTICS CODE HERE
+
+  var gaNotSet = () => { return ga_id === 'UA-xxx-yy' };
+
+  if (gaNotSet() && console){
+      console.log("NO GOOGLE ANALYTICS TRACKING CODE FOUND! ADD YOURS HERE: (/imports/helpers/ga.js)")
+  }
+  else if (typeof document !== 'undefined') {
     if(!document.querySelectorAll('[dochead="1"]')){
       var gaScript = 'https://www.google-analytics.com/analytics.js'
       DocHead.loadScript(gaScript, function () {
-        ga('create', 'UA-2437441-17', 'auto') // This is the tracking code for meteorkits.com - no use to you
+        ga('create', ga_id, 'auto')
         ga('send', 'pageview')
       })
     }
   }
-
-
 
 }
