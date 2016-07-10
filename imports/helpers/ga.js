1b508b613804c7c934bc19a086381384d3ca9ada
Matt Durr <matt@onedurr.com>
2016-07-10 19:27:26 -0400

    only show ga warning once, on startup

--- imports/helpers/ga.js
+++ imports/helpers/ga.js
@@ -1,13 +1,15 @@
+var ga_id = 'UA-xxx-yy'; // ADD YOUR GOOGLE ANALYTICS CODE HERE
 
-export let TrackingCode = () => {
-  var ga_id = 'UA-xxx-yy'; // ADD YOUR GOOGLE ANALYTICS CODE HERE
+var ga_set = () => { return ga_id !== 'UA-xxx-yy' };
 
-  var gaNotSet = () => { return ga_id === 'UA-xxx-yy' };
-
-  if (gaNotSet() && console){
+Meteor.startup(() => {
+  if (!ga_set() && console){
       console.log("NO GOOGLE ANALYTICS TRACKING CODE FOUND! ADD YOURS HERE: (/imports/helpers/ga.js)")
   }
-  else if (typeof document !== 'undefined') {
+})
+
+export let TrackingCode = () => {
+  if (ga_set() && typeof document !== 'undefined') {
     if(!document.querySelectorAll('[dochead="1"]')){
       var gaScript = 'https://www.google-analytics.com/analytics.js'
       DocHead.loadScript(gaScript, function () {
@@ -16,5 +18,4 @@ export let TrackingCode = () => {
       })
     }
   }
-
 }
